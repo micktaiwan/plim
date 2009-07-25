d85b66536ef47d2c32928cb966e8aba9acb2eaea
Mickael <faivrem@gmail.com>
2009-07-25 23:21:21 +0200

    jobs search

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -14,12 +14,37 @@ class JobsController < ApplicationController
   
   def create
     begin
-      Job.new_from_params(params[:job],current_user.company_id)
-      render :action => "create.js.rjs" 
+      p = params[:job]
+      job = Job.new
+      job.date        = p[:date]
+      job.company_id  = current_user.company_id
+      job.zone_id     = p[:zone].to_i    
+      job.employee_id = p[:employee].to_i    
+      job.user_id     = current_user.id    
+      job.type_id     = p[:job_type].to_i    
+      job.serial_id   = Serial.get_or_create(p[:serial])
+      job.phone_id    = Phone.get_or_create(p[:phone])
+      job.ampm        = p[:ampm] # FIXME check value
+      job.save!
+      #render :action => "create.js.rjs" 
     rescue Exception => e
       @e = e
       render :action => "error.js.rjs"
     end
+  end
+
+  def search
+    @zones      = Zone.find(:all,:conditions=>["company_id=?",current_user.company_id],:order=>"code")
+    @types      = JobType.find(:all,:conditions=>["company_id=?",current_user.company_id],:order=>"sort")
+    @employees  = Employee.find(:all,:conditions=>["company_id=?",current_user.company_id],:order=>"team")
+    @sort = [[I18n.t(:mdate),'date'], [I18n.t(:team),'employees.name']]
+  end
+  
+  def do_search  
+    sort = params[:sort]
+    @jobs = Job.find(:all, :include=>['zone','employee'], :order=>sort)
+    @results = render_to_string(:partial=>'results.erb')
+    #render :action => "do_search.js.rjs"
   end
 
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,23 +1,20 @@
 class Job < ActiveRecord::Base
 
-  has_one :zone
-  has_one :job_type
-  has_one :employee
-  has_one :serial
-  has_one :phone
+  belongs_to :zone
+  belongs_to :job_type
+  belongs_to :employee
+  belongs_to :serial
+  belongs_to :phone
   
-  def self.new_from_params(p, company_id)
-    job = new
-    job.date        = p[:date]
-    job.company_id  = company_id
-    job.zone_id     = p[:zone_id].to_i    
-    job.employee_id = p[:employee_id].to_i    
-    job.user_id     = p[:user_id].to_i    
-    job.type_id     = p[:type_id].to_i    
-    job.serial_id   = Serial.get_or_create(p[:serial])
-    job.phone_id    = Phone.get_or_create(p[:phone])
-    job.ampm        = p[:ampm] # FIXME check value
-    job.save!
+  before_save :verify
+  
+  def verify
+    # verify duplicate for this date, team, phone and serial
+    raise I18n.t(:job_already_exists) if  duplicate?
+  end
+  
+  def duplicate?
+    Job.find(:first, :conditions=>["date=? and phone_id=?", self.date, self.phone_id])
   end
   
 end
