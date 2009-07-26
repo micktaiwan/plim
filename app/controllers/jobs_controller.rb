47845a58390359296dcf0147e36dc66eb012248a
Mickael <faivrem@gmail.com>
2009-07-26 14:05:53 +0200

    search done

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -37,12 +37,47 @@ class JobsController < ApplicationController
     @zones      = Zone.find(:all,:conditions=>["company_id=?",current_user.company_id],:order=>"code")
     @types      = JobType.find(:all,:conditions=>["company_id=?",current_user.company_id],:order=>"sort")
     @employees  = Employee.find(:all,:conditions=>["company_id=?",current_user.company_id],:order=>"team")
-    @sort = [[I18n.t(:mdate),'date'], [I18n.t(:team),'employees.name']]
+    @sort = [[I18n.t(:mdate),'date'], [I18n.t(:team),'employees.team']]
   end
   
   def do_search  
     sort = params[:sort]
-    @jobs = Job.find(:all, :include=>['zone','employee'], :order=>sort)
+    fromdate  = params[:fromdate]
+    todate    = params[:todate]
+    zone      = params[:s][:zone]
+    job_type  = params[:s][:job_type]
+    team      = params[:s][:team]
+    serial    = params[:serial]
+    phone     = params[:phone]
+    cond = []
+    cond << "date>='#{fromdate}'"   if fromdate != ''
+    cond << "date<='#{todate}'"     if todate != ''
+    cond << "zone_id='#{zone}'"     if zone != ''
+    cond << "type_id='#{job_type}'" if job_type != ''
+    cond << "employee_id='#{team}'" if team != ''
+    if serial != ''
+      serial_id = Serial.find_by_serial(serial)
+      cond << "serial_id='#{serial_id.id}'" if serial_id
+      @results = I18n.t(:serial_not_found)
+      return
+    end
+    if phone != ''
+      phone_id = Phone.find_by_phone(phone)
+      cond << "phone_id='#{phone_id.id}'" if phone_id
+      @results = I18n.t(:phone_not_found)
+      return
+    end
+
+    
+    if cond.size > 0
+      @jobs = Job.find(:all, :conditions=>cond.join(" and "), :include=>['zone','employee'], :order=>sort)
+    else
+      @jobs = Job.find(:all, :include=>['zone','employee'], :order=>sort)
+    end
+    if @jobs.size == 0
+      @results = I18n.t(:no_record_found)
+      return
+    end
     @results = render_to_string(:partial=>'results.erb')
     #render :action => "do_search.js.rjs"
   end
