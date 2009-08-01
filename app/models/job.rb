5090d2fc803c18646d3e72d877e04c265ba96e81
Mickael <faivrem@gmail.com>
2009-08-01 16:56:07 +0200

    fix

--- app/models/job.rb
+++ app/models/job.rb
@@ -17,10 +17,30 @@ class Job < ActiveRecord::Base
     Job.find(:first, :conditions=>["date=? and phone_id=?", self.date, self.phone_id])
   end
   
+  def friendly_zone
+    return I18n.t(:nil_result) if not zone
+    zone.code + " " + zone.name
+  end
+
+  def friendly_serial
+    return I18n.t(:nil_result) if not serial
+    serial.serial
+  end
+
+  def friendly_phone
+    return I18n.t(:nil_result) if not phone
+    phone.phone
+  end
+
+  def friendly_job_type
+    return I18n.t(:nil_result) if not job_type
+    job_type.name
+  end
+
   def friendly_result
     return I18n.t(:nil_result) if not result
     result.name
   end
-
+  
 end
 
